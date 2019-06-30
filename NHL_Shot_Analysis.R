11276880f98541e177dff663102dc71f96410a4b
KyleKalisz <kylekalisz@gmail.com>
2019-06-30 19:25:54 -0400

    Inital Upload of nhl exploratory data

---
+++ NHL_Shot_Analysis.R
@@ -0,0 +1,87 @@
+# Finding the Best Shot in the NHL
+
+# Load Packages
+library("tidyverse")
+library("ggthemes")
+
+# Import Data
+
+nhl_data <- read.csv("2017-18 Data.csv", stringsAsFactors = FALSE, fileEncoding ="UTF-8-BOM")
+
+# Explore Data 
+
+# Goals
+nhl_goals <- select(nhl_data,"G.Bkhd", "G.Dflct", "G.Slap", "G.Snap", "G.Tip", "G.Wrap", "G.Wrst")
+
+total_goals <- sum(nhl_goals)
+
+backhand_goals <- sum(nhl_goals$G.Bkhd)
+deflection_goals <- sum(nhl_goals$G.Dflct)
+slap_goals <- sum(nhl_goals$G.Slap)
+snap_goals <- sum(nhl_goals$G.Snap)
+tip_goals <- sum(nhl_goals$G.Tip)
+wrap_goals <- sum(nhl_goals$G.Wrap)
+wrist_goals <- sum(nhl_goals$G.Wrst)
+
+sum_goals <- data.frame(Number_of_Goals = c(backhand_goals, deflection_goals, slap_goals, snap_goals, tip_goals, wrap_goals, wrist_goals),
+          Shot_Type = c("Backhand Goals", "Deflection Goals", "Slap Shot Goals", "Snap Shot Goals", "Tip Goals", "Warp Around Goals", "Wrist Shot Goals"))
+
+sum_goals$freq <- as.numeric(sum_goals[,1]) / total_goals
+
+ggplot(sum_goals, aes(x = reorder(Shot_Type,-freq), y = freq)) +
+      geom_bar(stat = "identity", fill = "skyblue4") +
+      labs(title = "Frequency of Goals Based off Shot Type", subtitle = "From the 2017-18 NHL Season", caption = "7,449 Goals Scored") +
+      xlab("Type of Shot") +
+      ylab("Frequency of Goals") +
+      theme_wsj() + 
+      scale_y_continuous(labels=scales::percent)
+
+# Shots On Goals
+
+nhl_shots <- select(nhl_data, "S.Bkhd", "S.Dflct", "S.Slap", "S.Snap", "S.Tip", "S.Wrap", "S.Wrst")
+
+backhand_shots <- sum(nhl_shots$S.Bkhd)
+deflection_shots <- sum(nhl_shots$S.Dflct)
+slap_shots <- sum(nhl_shots$S.Slap)
+snap_shots <- sum(nhl_shots$S.Snap)
+tip_shots <- sum(nhl_shots$S.Tip)
+wrap_shots <- sum(nhl_shots$S.Wrap)
+wrist_shots <- sum(nhl_shots$S.Wrst)
+
+
+sum_shots <- data.frame(Number_of_Shots = c(backhand_shots, deflection_shots, slap_shots, snap_shots, tip_shots, wrap_shots, wrist_shots),
+                        Shot_Type = c("Backhand Shots", "Deflection Shots", "Slap Shot Shots", "Snap Shot Shots", "Tip Shots", "Warp Around Shots", "Wrist Shot Shots"))
+
+total_shots <- sum(nhl_shots)
+
+# League Shooting %
+
+total_goals / total_shots
+
+# 9.17 %
+# Shot Type Chart
+
+sum_shots$freq <- as.numeric(sum_shots[,1]) / total_shots
+
+ggplot(sum_shots, aes(x = reorder(Shot_Type,-freq), y = freq)) +
+  geom_bar(stat = "identity", fill = "skyblue4") +
+  labs(title = "Frequency of Shots Based off Shot Type", subtitle = "From the 2017-18 NHL Season", caption = "81,228 Shots on Goal") +
+  xlab("Type of Shot") +
+  ylab("Frequency of Shots") +
+  theme_wsj() +
+  scale_y_continuous(labels=scales::percent)
+
+# Most Effective Shot
+
+sum_goals$ShootingPer <- as.numeric(sum_goals[,1]) / as.numeric(sum_shots[,1])
+
+ggplot(sum_goals, aes(x = reorder(Shot_Type,-ShootingPer), y = ShootingPer)) +
+  geom_bar(stat = "identity", fill = "skyblue4") +
+  labs(title = "Most Effective Shot", subtitle = "From the 2017-18 NHL Season") +
+  xlab("Type of Shot") +
+  ylab("Shooting Percentage") +
+  theme_wsj() +
+  scale_y_continuous(labels=scales::percent)
+
+
+    
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # NHL_Exploratory
 NHL 17-18 Season Data 
+
+All Data downlaoded from 
+http://www.hockeyabstract.com/testimonials/nhl2017-18
+
+Stats compiled by Rob Vollman, http://www.hockeyabstract.com
+Data sourced from NHL.com by Matt Cane, http://www.puckplusplus.com 
+
+	Data Sources:
+Data Sources:
+Primary data source is NHL official web site http://www.nhl.com
+Blank means that the column is calculated from other cells using a simple, common formula
+A lot of additional data came from Corsica Hockey http://www.corsica.hockey 
+Rebounds, rush shots, high-danger chances, and more came from Natural Stat Trick http://www.naturalstattrick.com 
+Salary cap data from Cap Friendly http://www.capfriendly.com 
+Equipment information from Gear Geek http://www.geargeek.com gathered by Evan Haldane
+Point Shares, career data, debut, seasons, and more from Hockey Reference http://www.hockey-reference.com
+Faceoff stats (NSPF) from Craig Tabita and Puck Base http://www.puckbase.com
+Injury data from Thomas Crawshaw of Springing Malik http://springingmalik.blogspot.ca
+Icings and crucial/critical points from More Hockey Stats http://www.morehockeystats.com 
+
+Original Concepts (where different from the data source):
+Expected goals (aka weighted shots) and rebound statistic innovated by Alan Ryder
+Corsi (SAT), Fenwick (USAT), Zone Starts named and popularized by Tim Barnes (aka Vic Ferrari)
+PDO (SPSV) named and popularized by Tim Barnes (aka Vic Ferrari) and Brian King
+Quality of competition and teammates by Gabriel Desjardins
+Rush shots statistic innovated by David Johnson
+This version of Wins Above Replacement (WAR) by Emmanuel Perry of Corsica Hockey
+Disciplined aggression proxy by Iain Fyffe
+Game Score by Dominik Luszczszyn
+Grit by Sportsnet
+Presence, Clean Play Points, and crucial/critical goal metrics by Jeff Z Klein and Karl-Eric Reif
+Point Shares by Justin Kubatko of Hockey Reference
+Block shot percentage by Derek Zona
+Pass, Net Penalty differential, IPP%, and more by Rob Vollman
+
